5cdd9c11fbfd81c755a64781b9657a6eee875fbf
episteme-debug <154646216+episteme-debug@users.noreply.github.com>
2025-05-17 18:51:48 -0500

    Primer commit del proyecto

---
+++ src/app/vendedor/page.tsx
@@ -0,0 +1,10 @@
+import { redirect } from "next/navigation";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Bienvenido a nuestra tienda</h1>
+      <p>Explora nuestros productos y disfruta de una experiencia de compra única.</p>
+    </div>
+  );
+}
