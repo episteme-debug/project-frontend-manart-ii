5274072d9af9b914ecac72c2963c9227381c0fb5
Artesanias Manart <somosartesaniasmanartmail.com>
2025-06-11 03:21:22 -0500

    inicios peticion

--- src/api/productos.ts
+++
@@ -1,7 +0,0 @@
-import { obtenerCookie } from "@/lib/ObtencionCookie"
-
-export default function listarProductosUsuario() {
-    const usuario = obtenerCookie()
-
-    if (!usuario) return "No autenticado";
-}
